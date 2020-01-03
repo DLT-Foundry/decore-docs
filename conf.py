fc92603ce529cb1c27a20cc2a64ab164c031bb46
cc-cara <cara@18778833932@163.com>
2020-01-03 11:47:06 +0800

    modified conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'caradecore'
-copyright = '2019, cara'
-author = 'cara'
+project = 'DECORE'
+copyright = '2019, Caesar Chad'
+author = 'Caesar Chad'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
